12f4ba7cc54ffeda3de40074f6784dd8baff9f13
Have Samuel <samiehave9@gmail.com>
2022-04-13 02:48:14 +0300

    Adding list items dynamically

--- modules/api.js
+++ modules/api.js
@@ -1,7 +1,7 @@
 const loadPage = async() => {
   try {
     const response = await fetch(
-      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games//scores/',
+      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Have-Samuel/scores/',
       {
         method: 'POST',
         body: JSON.stringify(),
@@ -12,7 +12,38 @@ const loadPage = async() => {
     );
     if(response.ok) {
       console.log(response);
-      const section = document.querySelectorAll('#grid');
+      const section = {
+        img: '../src/images/pic1.jpg',
+        title: 'Food 1',
+      };
+
+      function createPage() {
+        const oneSection = document.createElement('section');
+        const div = document.createElement('div');
+        div.classList = 'card-container';
+        oneSection.appendChild(div);
+
+        const imgDiv = document.createElement('div');
+        imgDiv.classList = 'card-pic';
+        div.appendChild(imgDiv);
+
+        const divMeta = document.createElement('div')
+        divMeta.classList = "card-meta";
+        div.appendChild(divMeta);
+
+        const divInfo = document.createElement('div');
+        divInfo.classList = 'card-info'
+        divMeta.appendChild(divInfo);
+
+        const divTitle = document.createElement('div');
+        divTitle.classList = 'card-title';
+        divInfo.appendChild(divTitle);
+
+        const likeImg = document.createElement('img')
+        likeImg.classList = 'like-btn';
+        divInfo.appendChild(likeImg);
+
+      }
      
     }
   }
