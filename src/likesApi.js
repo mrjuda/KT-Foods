36df3cc38a8c3b68fb8f0f5e2dd70ed107384728
Have Samuel <samiehave9@gmail.com>
2022-04-15 01:03:27 +0300

    Changes

--- src/likesApi.js
+++ src/likesApi.js
@@ -1,39 +1,17 @@
-/* eslint-disable operator-linebreak */
-/* eslint-disable no-unused-vars */
-const invURL =
-  // eslint-disable-next-line quotes
-  `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/5P3Y5z2n9UB0UfjvnCPt/likes`;
+const shows = [];
+const likes = [];
+const involvementID = 'GjywrG1wwhuquKQgcAen';
 
-// eslint-disable-next-line import/prefer-default-export
-export const setLikes = async (id) => {
-  const headers = new Headers();
-  headers.append('content-Type', 'application/json');
-
-  const newinput = JSON.stringify({ item_id: id });
-
-  const requestOptions = {
-    method: 'POST',
-    headers,
-    body: newinput,
-    redirect: 'follow',
-  };
-
-  await fetch(invURL, requestOptions)
-    .then((response) => response.json())
-    .then((result) => result)
-    .catch((error) => error);
+const involvementEndpoints = {
+  likes: `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/${involvementID}/likes`,
+  comments: `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/${involvementID}/comments`
 };
 
-export const getLikes = async () => {
-  try {
-    const requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-    const response = await fetch(invURL, requestOptions);
-    const likes = await response.json();
-    return likes;
-  } catch (error) {
-    return [];
-  }
+const showEndpoints = {
+  shows: 'https://api.themealdb.com/shows',
 };
+
+export default {
+  shows, likes, involvementEndpoints, showEndpoints,
+};
+
