5c8546ef39cd258edbee5f34457ae01589af0211
Have Samuel <samiehave9@gmail.com>
2022-04-16 08:18:24 +0300

    Testing counter

---
+++ counter.test.js
@@ -0,0 +1,37 @@
+const getCount = require('./modules/counter.js');
+
+const items = [
+  {
+    name: 'Have',
+    price: 50,
+  },
+  {
+    name: 'Have',
+    price: 50,
+  },
+  {
+    name: 'Have',
+    price: 50,
+  },
+  {
+    name: 'Have',
+    price: 50,
+  },
+  {
+    name: 'Have',
+    price: 50,
+  },
+  {
+    name: 'Have',
+    price: 50,
+  },
+  {
+    name: 'Have',
+    price: 50,
+  },
+];
+
+test('returns item count', async () => {
+  expect(await getCount(items, 5)).toBe(5);
+  expect(await getCount(items, 3)).toBe(3);
+});
